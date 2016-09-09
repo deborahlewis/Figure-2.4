fcb51151ba3f9142c1b9c4cd97a5cf939815e087
Deborah Lewis <deborahlewis@users.noreply.github.com>
2016-09-09 10:48:45 -0400

    Add files via upload

---
+++ Deitel_Figure2_4.java
@@ -0,0 +1,17 @@
+/*
+ * Deborah Lewis
+ * Figure 2.5 Deitel: How to Program (Early Ojects)
+ * Printing multiple lines of text with a single statement
+ */
+package deitel_figure2_4;
+
+public class Deitel_Figure2_4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Welcome\nto\nJava\nProgramming!");
+    } //end main
+    
+} //end class
